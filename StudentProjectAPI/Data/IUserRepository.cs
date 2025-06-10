14f25c45912db5f482b07610d7a30cd1b600d49b
Adzekodzovi <154748092+Adzekodzovi@users.noreply.github.com>
2025-06-10 13:00:57 +0000

    IUserRepository.cs

--- StudentProjectAPI/Data/IUserRepository.cs
+++ StudentProjectAPI/Data/IUserRepository.cs
@@ -5,7 +5,10 @@ namespace StudentProjectAPI.Data
     public interface IUserRepository
     {
         Task<User?> GetByIdAsync(int id);
+        Task<User?> GetByEmailAsync(string email);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<bool> AddAsync(User user);
+        Task<bool> UpdateAsync(User user);
         Task<bool> DeleteAsync(int id);
-        // Ajoutez d'autres méthodes au besoin
     }
 }
